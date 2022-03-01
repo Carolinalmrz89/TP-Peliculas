5d97c250eb930143cca1b69a1c35c86563d5540d
Carolinalmrz89 <lais.carolina.almaraz89@gmail.com>
2022-03-01 18:54:59 -0300

    funcionalidad input search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,51 @@
+import { useSearchParams } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import Box from "@mui/material/Box";
+import { useState } from "react";
+
 const Search = () => {
+  const [searchParams, setSearchParams] = useSearchParams({
+    query: "",
+  });
+
+  const [valorDelInput, setValorDelInput] = useState("");
+
+  const handleChange = (e) => {
+    setValorDelInput(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setSearchParams({
+      query: valorDelInput,
+    });
+  };
+
   return (
-    <div>
-      <h2>Search</h2>
-    </div>
+    <Box sx={{ display: "flex", justifyContent: "center", m: 5 }}>
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", width: 400, display: "flex" }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search movie"
+          onChange={handleChange}
+          value={valorDelInput}
+        />
+        <IconButton
+          type="submit"
+          sx={{ p: "10px" }}
+          aria-label="search"
+          onClick={handleClick}
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </Box>
   );
 };
 
