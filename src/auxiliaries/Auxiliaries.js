e13549343fc5a131900f0f16dd5790100f7ff852
Carolinalmrz89 <lais.carolina.almaraz89@gmail.com>
2022-03-20 00:17:24 -0300

    se agregan botones paginado en ultimos lanzamientos y populares

--- src/auxiliaries/Auxiliaries.js
+++ src/auxiliaries/Auxiliaries.js
@@ -1,2 +1,3 @@
 export const urlBase = "https://api.themoviedb.org/3/movie/";
 export const apiKey = "0a68557b9bafe8081e017b64d2ebdb4e";
+export let actualPage = 1;
--- src/components/SectionsPopularLatest.js
+++ src/components/SectionsPopularLatest.js
@@ -2,13 +2,18 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
-import { urlBase, apiKey } from "../auxiliaries/Auxiliaries";
+import { urlBase, apiKey, actualPage } from "../auxiliaries/Auxiliaries";
+import Button from "@mui/material/Button";
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 
 const SectionsPopularLatest = ({ title, url }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch(`${urlBase}${url}?api_key=${apiKey}`)
+    fetch(
+      `${urlBase}${url}?api_key=${apiKey}&lenguage=es-AR&page=${actualPage}`
+    )
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results);
@@ -40,6 +45,25 @@ const SectionsPopularLatest = ({ title, url }) => {
             link={`/movie-detail/${movie.id}`}
           />
         ))}
+      </Box>
+      <Box sx={{ display: "flex" }}>
+        <Button
+          variant="outlined"
+          startIcon={<ArrowBackIosNewOutlinedIcon />}
+          sx={{ m: 2 }}
+        >
+          Prev
+        </Button>
+        <Typography variant="h6" component="div" sx={{ m: 2 }}>
+          Page {actualPage}
+        </Typography>
+        <Button
+          variant="outlined"
+          endIcon={<ArrowForwardIosOutlinedIcon />}
+          sx={{ m: 2 }}
+        >
+          Next
+        </Button>
       </Box>
     </Box>
   );
