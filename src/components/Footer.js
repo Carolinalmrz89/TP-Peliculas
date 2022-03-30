e8cf303bcb4d446d33969b6166a366dec0880b55
Carolinalmrz89 <lais.carolina.almaraz89@gmail.com>
2022-03-29 21:23:31 -0300

    se agrega contenido y estilos en footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,58 @@
+import Paper from "@mui/material/Paper";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import { mainColor } from "../auxiliaries/Auxiliaries";
+import { Typography } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 const Footer = () => {
-  return <h2>Footer</h2>;
+  return (
+    <Paper
+      sx={{
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+      elevation={3}
+    >
+      <BottomNavigation
+        showLabels
+        sx={{
+          bgcolor: mainColor,
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <Box>
+          <Typography variant="h6" component="div" sx={{ color: "white" }}>
+            {" "}
+            LMRZ DEV - 2022
+          </Typography>
+        </Box>
+        <Box>
+          <Link href="https://github.com/Carolinalmrz89">
+            <BottomNavigationAction
+              label="Recents"
+              icon={<GitHubIcon />}
+              sx={{ color: "white", p: 0, pt: 2, fontSize: "30pxs" }}
+            />
+          </Link>
+          <Link href="https://www.linkedin.com/in/carolina-almaraz/">
+            <BottomNavigationAction
+              label="Recents"
+              icon={<LinkedInIcon />}
+              sx={{ color: "white", p: 0, pt: 2, fontSize: "30pxs" }}
+            />
+          </Link>
+        </Box>
+      </BottomNavigation>
+    </Paper>
+  );
 };
 
 export default Footer;
